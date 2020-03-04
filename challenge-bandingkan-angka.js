a87bb46a7e69cec43f0d2f28cd3d2c15dca3b58e
syukur <syukurbaru@gmail.com>
2020-03-04 15:49:10 +0700

    update banding angka dan xo

--- challenge-bandingkan-angka.js
+++ challenge-bandingkan-angka.js
@@ -1,14 +1,13 @@
 function bandingkanAngka(angka1, angka2){
-if(angka2 > angka1){
-    return true;
-} else if(angka2 < angka1){
-    return false;
-} else if(angka2 = angka1){
-    return -1;
-}
-
-
-}
+    if(angka2 > angka1){
+        return true;
+    } 
+    else if(angka2 < angka1){
+        return false;
+    } else if(angka2 = angka1){
+        return -1;
+        }
+    }
 
 console.log(bandingkanAngka(5,8)); // true
 console.log(bandingkanAngka(5, 3)); // false
